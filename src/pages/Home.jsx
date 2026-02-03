87ac0afb3029a375c8d5fb0ef5eab9827afe2898
Anushka Maity <maityanushka60@gmail.com>
2026-02-03 11:50:46 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='text-3xl'>
-      This is home page and I love You :)
-      I love you tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo :)
+      
     </div>
   )
 }
