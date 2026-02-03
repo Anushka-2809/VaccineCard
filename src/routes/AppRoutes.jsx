f75df6ddf6308925476d298b843304d99896962e
Anushka Maity <maityanushka60@gmail.com>
2026-02-03 20:13:48 +0530

    routing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-
+import PatientDashboard from "../pages/PatientDashboard";
+import DoctorDashboard from "../pages/DoctorDashboard";
+import VaccinationRecord from "../pages/VaccinationRecord";
+import QRcode from "../pages/QRcode";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/patient" element={<PatientDashboard />} />
+      <Route path="/doctor" element={<DoctorDashboard />} />
+      <Route path="/records" element={<VaccinationRecord />} />
+      <Route path="/qrcode" element={<QRcode />} />
+    </Routes>
   );
 };
 
